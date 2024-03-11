c71bd57d4b2b5f6917017406500b639c43c8bc80
FelixVoelker <fel.voelker@googlemail.com>
2024-03-11 15:07:55 +0100

    Added exercise material.

---
+++ Übungen/stein_schere_papier.py
@@ -0,0 +1,14 @@
+""" Übung 1: Stein Schere Papier
+
+    Lass den Spieler im Spiel "Stein Schere Papier" gegen den Computer
+    antreten. Dieser soll seine Handzeichen zufällig auswählen.
+
+    Folgende Darstellung der Handzeichen kannst du verwenden:
+
+        1 - Stein
+        2 - Schere
+        3 - Papier
+
+    Lies die Wahl des Spielers über das Terminal ein und gib aus, ob
+    der Spieler gewonnen hat oder nicht.
+"""
