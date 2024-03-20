a0779720c22722c741060b827d6d4a4d425988ed
FelixVoelker <fel.voelker@googlemail.com>
2024-03-20 12:33:23 +0100

    Added exercise material for quiz game.

---
+++ Übungen/uebung2.py
@@ -0,0 +1,10 @@
+""" Übung 2: Quiz
+
+    Erstelle ein Quiz Spiel mit Objektorientierter Programmierung (OOP).
+    Überlege dir dafür ein Konzept wie Fragen und Antwortmöglichkeiten
+    als Objekte dargestellt werden können.
+
+    Schreibe 5 Objekte dann hintereinander in das Terminal und prüfe dabei
+    ob der Spieler die Frage richtig beantwortet hat. Gib die Anzahl der
+    korrekten Antworten am Ende des Spiels aus.
+"""
